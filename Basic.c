bd06fefbdee01505b48b90594e01adb124ac06ab
Kou Kr <subratsubedi16@gmail.com>
2026-02-03 22:03:22 +0545

    sorting algorithm

--- Basic.c
+++ Basic.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <conio.h>
-int main() {
-    printf("Hello, World!\n");
-    fflush(stdout);
-    getch();
-    return 0;
+int main() 
+{
+int array[5]  = {1, 2, 3, 4, 5};
+for(int i = 1; i<=5; i++) {
+{for (int j = 0; j < 5; j++) 
+ {
+ if(array[j]>array[i])
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
+for (size_t i = 0; i < 5; i++)
+{
+    printf("%d ", array[i]);
+}
+
+}
+}  
